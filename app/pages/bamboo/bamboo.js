b2897a8bf0c2a029b0f194c8397d021b7020ffa9
beneapp <beneapp@gmail.com>
2016-05-23 08:57:33 +0900

    수정중

--- app/pages/bamboo/bamboo.js
+++ app/pages/bamboo/bamboo.js
@@ -1,5 +1,6 @@
 import {Page, NavController} from 'ionic-angular';
 import {BambooWritePage} from '../bamboo-write/bamboo-write'
+
 //import {FirebaseRef} from 'angularfire2';
 /*
   Generated class for the BambooPage page.
@@ -9,27 +10,60 @@ import {BambooWritePage} from '../bamboo-write/bamboo-write'
 */
 @Page({
   templateUrl: 'build/pages/bamboo/bamboo.html'
-  , providers: [Firebase]
 })
 export class BambooPage {
 
+
   static get parameters() {
-    return [[NavController]]; 
+    return [[NavController]];
   }
 
   constructor(nav) {
     this.nav = nav;
-    this.firebase = new Firebase("https://sweltering-heat-9516.firebaseio.com/2016/bamboo/");
+
+    //this.firebase = new Firebase("https://sweltering-heat-9516.firebaseio.com/2016/bamboo/root");
     this.title = "Say Anything";
     this.parent = "root";
-    
-    this.firebase.call();
+
+
+
+    // Initialize Firebase
+    var config = {
+      apiKey: "AIzaSyAFb6L3F0YMW0XJl6NdZGP7wtoWfa5naPY",
+      authDomain: "myfirstapp-bcfdc.firebaseapp.com",
+      databaseURL: "https://myfirstapp-bcfdc.firebaseio.com",
+      storageBucket: "myfirstapp-bcfdc.appspot.com",
+    };
+    var token = {
+      "provider": "anonymous",
+      "uid": "3c4e577c-d713-4561-b903-07c012f78a80"
+    };
+
+    this.fb = firebase.initializeApp(config);
+    debugger;
+    this.fb.auth(token).signInAnonymously().then((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.log("#E# " + errorCode + ":" + errorMessage);
+      this.database = this.fb.database();
+      this.database.ref('test/' + 'sth1').set({
+        username: 'name42222',
+        email: 'email43222'
+      });
+    });
+
+
+
+    //this.fb = firebase.intializeApp(config);
+    // console.log(config);
+    // this.fb = firebase.intializeApp(config);
+
+
     //this.myDataRef = new Firebase("https://sweltering-heat-9516.firebaseio.com/2016/bamboo/");
     // //this.bookItems = af.list('');
     // //this.myDataRef = ref;
-
     // //데이터를 가져온다.
-    // this.myDataRef.once('value',
+    // this.firebase.once('value',
     //   (snapshot) => {
     //     console.log(snapshot.val());
     //     var items = snapshot.val();
@@ -39,7 +73,7 @@ export class BambooPage {
     //   }, error => console.log(error.code)
     //   , () => console.log('read complete')
     // )
-        
+
   }
 
   eventHandler(event) {
@@ -48,10 +82,10 @@ export class BambooPage {
     // var element = document.getElementById("TextArea");    
     // element.style.height = element.scrollHeight + "px";
     // debugger;
-   
+
   }
-  
-  Reply(){
-    this.nav.push(BambooWritePage, {title:this.title, level:this.level});
+
+  add() {
+    this.nav.push(BambooWritePage, { title: this.title, level: this.level });
   }
 }
